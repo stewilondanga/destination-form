6bf10cd4fba7d289e43cbe6c49c1912fd7547d44
Stewart ilondanga <stilondanga@gmail.com>
2019-06-22 22:21:35 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -32,21 +32,20 @@ function scroll() {
 }
 scroll();
 // mymap.setView([0, 100], 4);
-/*$.fn.center = function () {
-  this.css("position","absolute");
+$.fn.center = function() {
+  this.css("position", "absolute");
   this.css("top", Math.max(0, (
-    ($(window).height() - $(this).outerHeight()) / 2) +
-     $(window).scrollTop()) + "px"
-  );
+      ($(window).height() - $(this).outerHeight()) / 2) +
+    $(window).scrollTop()) + "px");
   this.css("left", Math.max(0, (
-    ($(window).width() - $(this).outerWidth()) / 2) +
-     $(window).scrollLeft()) + "px"
-  );
+      ($(window).width() - $(this).outerWidth()) / 2) +
+    $(window).scrollLeft()) + "px");
   return this;
 }
- $("#map-form").hide();
+$("#map-form").hide();
 $("#overlay-content").show().center();
-function register(){
+
+function register() {
   $("#overlay-content").hide();
   $("#map-form").show().center();
   console.log("hey");
